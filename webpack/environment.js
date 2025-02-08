6490d8f56766d9ebbcf694dc1a39bab96b2efb53
Dominique Haroun <haroundominique@gmail.com>
2025-02-08 15:28:53 +0100

    modificando de nuevo endpoint en produccion otra vez

--- webpack/environment.js
+++ webpack/environment.js
@@ -1,5 +1,5 @@
 module.exports = {
   I18N_HASH: 'generated_hash',
-  SERVER_API_URL: 'https://pickall.netlify.app',
+  SERVER_API_URL: 'https://pickall.netlify.app/',
   __VERSION__: process.env.APP_VERSION || 'DEV',
 };
