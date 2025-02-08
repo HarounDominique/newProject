17672567f6383df06ebc67cbd167aa1c9d07053d
Dominique Haroun <haroundominique@gmail.com>
2025-02-08 16:52:35 +0100

    apuntando a localhost8080

--- webpack/environment.js
+++ webpack/environment.js
@@ -1,5 +1,6 @@
 module.exports = {
   I18N_HASH: 'generated_hash',
-  SERVER_API_URL: 'https://pickall.netlify.app/',
+  //SERVER_API_URL: 'https://pickall.netlify.app/',
+  SERVER_API_URL: 'https://localhost:8080/',
   __VERSION__: process.env.APP_VERSION || 'DEV',
 };
